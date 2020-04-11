dc044185e84143d9f337b8654220dca31adaf59d
Alexis Lahouze <alexis@lahouze.org>
2020-04-11 22:04:49 +0200

    Set default identities to /home/alahouze/.ssh/id_rsa.

--- conf.d/ssh-agent.fish
+++ conf.d/ssh-agent.fish
@@ -2,5 +2,4 @@
 
 # Symlink is /tmp/ssh-agent-$USER.sock
 
-echo "set identities"
-set --global identities $HOME/.ssh/xals.rsa $HOME/.ssh/HOME/.ssh/xals-old.rsa $HOME/.ssh/alexis@sysnove.fr
+set --global identities $HOME/.ssh/id_rsa
